57042694aacd850b0530b5a80b06dd17d64f90b1
De Paula <luizh_@live.com>
2020-02-09 22:10:32 -0300

    favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -94,7 +94,7 @@ module.exports = {
         background_color: `#F6FBFB`,
         theme_color: `#F6FBFB`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/fav.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-sitemap`,
